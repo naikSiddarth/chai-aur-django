960c38af9f009b2d8658bf79a91057caa51fb495
Siddarth Naik <siddarthnaik2131@gmail.com>
2024-09-15 22:30:10 +0530

    Add templates for home

--- chaiAurDjango/chaiAurDjango/views.py
+++ chaiAurDjango/chaiAurDjango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("You are in chai aur django home page")
+    #return HttpResponse("You are in chai aur django home page")
+    return render(request,"index.html")
 
 def about(request):
     return HttpResponse("You are in chai aur django about page")

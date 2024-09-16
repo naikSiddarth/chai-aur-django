b95fdc3b65433fe45a5eec7f79477502f7da2079
Siddarth Naik <siddarthnaik2131@gmail.com>
2024-09-16 18:29:01 +0530

    Add home page to chai app

--- chaiAurDjango/chaiAurDjango/views.py
+++ chaiAurDjango/chaiAurDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     #return HttpResponse("You are in chai aur django home page")
-    return render(request,"index.html")
+    return render(request,"website/index.html")
 
 def about(request):
     return HttpResponse("You are in chai aur django about page")

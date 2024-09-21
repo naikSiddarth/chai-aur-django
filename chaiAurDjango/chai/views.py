606c4842b9e2ece7c5cd9a2f723f2ef9f066a891
Siddarth Naik <siddarthnaik2131@gmail.com>
2024-09-21 19:34:41 +0530

    add code to send data to frontend and iterate through it and display

--- chaiAurDjango/chai/views.py
+++ chaiAurDjango/chai/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import ChaiType
 
 # Create your views here.
 def all_chai(request):
-    return render(request,"chai/all_chai.html")
+    chais = ChaiType.objects.all()
+    return render(request,"chai/all_chai.html",{'chais':chais})
